55bad0e82d449e44f400eab1c69e3c2993e2271b
pgarg <pgarg@gridgain.com>
2017-09-05 09:54:16 -0700

    change config file path

--- src/main/java/ignite/myexamples/IgniteNodeStartup.java
+++ src/main/java/ignite/myexamples/IgniteNodeStartup.java
@@ -6,7 +6,7 @@ import org.apache.ignite.Ignition;
 public class IgniteNodeStartup {
 
     public static void main(String[] args) throws IgniteException {
-        Ignition.start("/Users/prachig/myexamples/config/cluster-config.xml");
+        Ignition.start("/Users/prachig/logger-example/config/cluster-config.xml");
 
     }
 }
--- src/main/java/ignite/myexamples/LoggerExample.java
+++ src/main/java/ignite/myexamples/LoggerExample.java
@@ -5,7 +5,7 @@ import org.apache.ignite.*;
 public class LoggerExample {
 
     public static void main(String[] args) throws IgniteException {
-        try (Ignite ignite = Ignition.start("/Users/prachig/myexamples/config/cluster-config.xml")) {
+        try (Ignite ignite = Ignition.start("/Users/prachig/logger-example/config/cluster-config.xml")) {
             ignite.log().info("Info Message 1 Logged !!!");
             ignite.log().info("Info Message 2 Logged !!!");
 
